dc211330b9bcd5ee4e6014ada2b203ff20a291e3
Mohammad Arsalan <arsalancode@outlook.com>
2018-03-19 01:50:54 +0530

    Added sample code for singleton pattern.

---
+++ Design Patterns/SingletonPattern.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Design_Patterns
+{
+    public class SingletonPattern
+    {
+        public void TestSingletonPattern()
+        {
+            Logger logger1 = Logger.GetInstance();
+            Logger logger2 = Logger.GetInstance();
+
+            logger1.Log();
+            logger2.Log();
+        }
+    }
+
+    public class Logger
+    {
+        private static Logger logger;
+
+        private Logger()
+        {
+
+        }
+
+        public void Log()
+        {
+
+        }
+
+        public static Logger GetInstance()
+        {
+            if (logger == null)
+            {
+                logger = new Logger();
+                return logger;
+            }
+            else
+            {
+                return logger;
+            }
+        }
+    }
+}
