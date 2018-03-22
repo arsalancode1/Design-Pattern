366df6d8cb2645e9c204b4f76648bd9997b8f6f8
Mohammad Arsalan <arsalancode@outlook.com>
2018-03-23 01:34:31 +0530

    added strategy pattern.

---
+++ Design Patterns/StrategyPattern.cs
@@ -0,0 +1,55 @@
+﻿namespace Design_Patterns
+{
+    class StrategyPattern
+    {
+        
+        public void TestStrategyPattern()
+        {
+            MathOperation mathOperation = new MathOperation(new AddOperationStrategy());
+            mathOperation.SetNumber(1,2);
+            mathOperation.GetResult();
+        }
+    }
+
+    public class MathOperation
+    {
+        IOperationStrategy operationStrategy;
+        int num1, num2;
+        public MathOperation(IOperationStrategy strategy)
+        {
+            this.operationStrategy = strategy;
+        }
+
+        public void SetNumber(int num1,int num2)
+        {
+            this.num1 = num1;
+            this.num1 = num2;
+        }
+        public int GetResult()
+        {
+            return operationStrategy.Operation(num1, num2);
+        }
+    }
+
+    public interface IOperationStrategy
+    {
+        int Operation(int num1, int num2);
+    }
+
+    public class AddOperationStrategy : IOperationStrategy
+    {
+        public int Operation(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+    }
+
+    public class SubstractOperationStrategy : IOperationStrategy
+    {
+        public int Operation(int num1, int num2)
+        {
+            return num1 - num2;
+        }
+    }
+
+}
